f11b8e9b8f23f37d950f3a8d2eb3152dca85ad51
twopats <lpatipat@icloud.com>
2023-08-15 02:46:29 -0400

    improve env loading

--- main.go
+++ main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"log"
+	"meg-server/controllers"
+	"os"
+
 	"github.com/gin-gonic/gin"
 	_ "github.com/joho/godotenv/autoload"
-  "meg-server/controllers"
 )
 
 func main() {
   // router
   r := gin.Default()
 
+
+  port := os.Getenv("PORT")
+  if port == "" {
+    log.Fatal("PORT not set in .env file")
+  }
+
   r.GET("/api/ping", controllers.Ping)
 
-  r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+  r.Run(":" + port) // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
 
