2721244951cdb02f6c442123cb4b501e4c760dce
twopats <lpatipat@icloud.com>
2023-08-15 01:58:45 -0400

    set up init routes and air for live refresh

---
+++ controllers/chat.go
@@ -0,0 +1,12 @@
+package controllers
+
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+func Chat(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"message": "text",
+	})
+}
--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
+	_ "github.com/joho/godotenv/autoload"
+  "meg-server/controllers"
 )
 
 func main() {
+  // router
   r := gin.Default()
-  r.GET("/ping", func(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{
-      "message": "pong",
-    })
-  })
+
+  r.GET("/api/ping", controllers.Ping)
+
   r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
+
